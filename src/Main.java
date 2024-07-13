1dc23f739dcfc81dd48b45ddc79b3f557f873596
arwasaleem <arwasaleem.2@gmail.com>
2024-07-12 21:23:23 -0400

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.sql.SQLOutput;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        // System.out.println("Hello world!");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Arwa is the Best!");
+            Thread.sleep(2000);
+        }
+
     }
 }
+
